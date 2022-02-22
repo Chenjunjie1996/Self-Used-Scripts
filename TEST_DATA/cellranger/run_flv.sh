59d60c3228ebfe4554f228ccd5c3137c4e0343ed
陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>
2022-02-22 13:48:36 +0800

    testdata

---
+++ TEST_DATA/cellranger/run_flv.sh
@@ -0,0 +1,8 @@
+multi_vdj_full_len \
+    --mapfile  ./test.mapfile \
+    --chemistry flv \
+  --mem 10\
+  --species hs\
+  --thread 8\
+  --allowNoLinker\
+ --soft 4.0.0 --seqtype TCR --mod sjm
---
+++ TEST_DATA/trust/run_flv.sh
@@ -0,0 +1,8 @@
+multi_trust_vdj \
+	--mapfile  ./test.mapfile \
+	--chemistry flv \
+	--species hg38 \
+	--thread 8 \
+	--allowNoLinker \
+	--seqtype TCR \
+	--mod sjm
