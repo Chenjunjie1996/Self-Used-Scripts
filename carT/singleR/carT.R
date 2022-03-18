43d930f8b58dca895d0b892cce8fe50c9d96247e
陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>
2022-03-18 14:20:30 +0800

    singleR cart

---
+++ carT/singleR/carT.R
@@ -0,0 +1,74 @@
+library(Seurat)
+library(tidyverse)
+library(argparse)
+library(ggplot2)
+library(stringr)
+library(dplyr)
+
+parser <- ArgumentParser()
+parser$add_argument("--sample",help="sample name", required=TRUE)
+parser$add_argument("--rds", help="scRNA rds path", required=TRUE)
+parser$add_argument("--UMI_tsne", help='umi or tsne file', required=TRUE)
+parser$add_argument("--outdir", help='out dir', required=TRUE)
+parser$add_argument("--meta", help='meta', required=TRUE)
+args <- parser$parse_args()
+
+rds <- readRDS(args$rds)
+singleR_meta <- read.table(args$meta, sep='\t')
+rds@meta.data = singleR_meta
+# Idents(rds) <- "orig.ident"
+# rds <- subset(rds,idents = c(args$sample))
+
+if (grepl("UMI.csv", args$UMI_tsne)){
+  print(args$UMI_tsne)
+  cart <- read.table(args$UMI_tsne, sep=',', header=T)
+  # cart$shi<-paste(args$sample,"_",sep="")
+  # cart$barcode<-str_c(cart$shi,cart$barcode)
+  cells <- subset(cart, sd41bb_CD3zetaV2!=0)
+  barcodes <- unique(cells$barcode)
+}else{
+  print(args$UMI_tsne)
+  cart <- read_tsv(args$UMI_tsne)
+  # cart$shi<-paste(args$sample,"_",sep="")
+  # cart$barcode<-str_c(cart$shi,cart$barcode)
+  cells <- subset(cart, tag!=0)
+  barcodes <- unique(cells$barcode)
+}
+
+df <- rds@meta.data
+df$barcode <- rownames(df)
+
+filter_df <- filter(df, barcode %in% barcodes)
+res <- table(filter_df$celltype)
+res <- as.data.frame(res)
+
+out_csv = stringr::str_glue("{args$outdir}/{args$sample}_match_barcodes_celltypes_distribution.txt")
+write_csv(res,file=out_csv)
+
+meta = rds@meta.data
+meta$Class = 'NA'
+meta[barcodes,'Class'] = 'positive'
+meta <- meta %>% drop_na(nCount_RNA)
+table(meta$Class == 'positive')
+rds@meta.data = meta
+
+out.df = stringr::str_glue("{args$outdir}/{args$sample}_metadata.tsv")
+write.table(as.data.frame(rds@meta.data), file=out.df, sep='\t')
+
+outP = stringr::str_glue("{args$outdir}/{args$sample}_cluster_umap.png")
+png(outP, height=1000, width=1000)
+UMAPPlot(rds,group.by='seurat_clusters',label=TRUE)
+dev.off()
+
+outP1 = stringr::str_glue("{args$outdir}/{args$sample}_umapplot.png")
+png(outP1, height=1000, width=1000)
+UMAPPlot(rds,group.by='Class',cols=c('grey','red'))
+dev.off()
+
+outP2 = stringr::str_glue("{args$outdir}/{args$sample}_assign.png")
+png(outP2, height=1000, width=1000)
+UMAPPlot(rds,group.by='celltype',label=TRUE,label.box=TRUE)
+dev.off()
+
+print(res)
+print(table(meta$celltype))
---
+++ carT/singleR/carT.py
@@ -0,0 +1,67 @@
+import os
+import subprocess
+import glob
+import pandas as pd
+import sys
+import xlwt
+import os
+
+def run_plot(rds, UMI_tsne, sample, outdir, meta):
+    cmd = (
+        f'Rscript /SGRNJ03/randd/cjj/Script/carT/singleR/carT.R '
+        f'--rds {rds} '
+        f'--UMI_tsne {UMI_tsne} '
+        f'--sample {sample} '
+        f'--outdir {outdir} '
+        f'--meta {meta} '
+    )
+    subprocess.check_call(cmd, shell=True)
+    
+
+def run_count(outdir, sample):
+    df_meta = pd.read_csv(f'{outdir}/{sample}_metadata.tsv', sep='\t')
+
+    report = xlwt.Workbook()
+    sheet = report.add_sheet('CART-Count')
+    row0 = [f"{sample}_celltype", "Cluster", "CellNumber", "Cell With CAR"]
+
+    cell_type_list = list(df_meta['celltype'].unique())
+    cluster_list, cell_number_list, car_cell_number_list = [], [], []
+
+    for i in cell_type_list:
+        cluster = sorted(df_meta[df_meta['celltype'] == f'{i}'].seurat_clusters.unique().tolist())
+        cluster_list.append(",".join([str(int(x)) for x in cluster]))
+        cell_number = df_meta[df_meta['celltype'] == f'{i}'].shape[0]
+        cell_number_list.append(cell_number)
+        car_cell_number = df_meta[ (df_meta['Class'] == 'positive') & (df_meta['celltype'] == f'{i}') ].shape[0]
+        car_cell_number_list.append(car_cell_number)
+
+    for _i in range(len(row0)):
+        sheet.write(0, _i, row0[_i])
+    for _i in range(len(cell_type_list)):
+        sheet.write(_i+1, 0, cell_type_list[_i])
+        sheet.write(_i+1, 1, cluster_list[_i])
+        sheet.write(_i+1, 2, cell_number_list[_i])
+        sheet.write(_i+1, 3, car_cell_number_list[_i])
+
+    report.save(f'{outdir}/{sample}_CART_count.xls')
+    return report
+    
+    
+def main():
+    sample, rds, UMI_tsne = sys.argv[1], sys.argv[2], sys.argv[3]
+    outdir = f"./CART_{sample}"
+    os.system(f"mkdir {outdir}")
+    meta = glob.glob(f"{os.path.dirname(rds)}/*_metadata.tsv")[0]
+    run_plot(rds, UMI_tsne, sample, outdir, meta)
+    run_count(outdir, sample)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
