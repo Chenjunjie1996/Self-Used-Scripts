17aebe5dc57e9f9b3415dad97c10d7df5e719d14
陈俊杰 <chenjunjie@SGRNJlogin.sgr.com>
2022-07-13 17:23:20 +0800

    get_anno_seq

---
+++ get_anno_seq/anno_seq.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import pysam
+import sys
+import copy
+import glob
+import matplotlib.pyplot as plt
+
+
+def get_anno_seq(cr_out):
+    airr = pd.read_csv(glob.glob(f'{cr_out}/03.assemble/*/outs/airr_rearrangement.tsv')[0],sep='\t')
+    contig = pd.read_csv(glob.glob(f'{cr_out}/03.assemble/*/outs/filtered_contig_annotations.csv')[0])
+    clonotype = pd.read_csv(glob.glob(f'{cr_out}/03.assemble/*/outs/clonotypes.csv')[0])
+    
+    contig = contig[contig['productive']==True]
+    contig_for_plot = copy.deepcopy(contig)
+    contig = contig[['contig_id','raw_clonotype_id','cdr3']]
+    contig.rename(columns={'contig_id':'sequence_id'},inplace=True)
+    df_merge = pd.merge(airr,contig,on='sequence_id',how='inner')
+    del df_merge['clone_id']
+    df_merge.rename(columns={'raw_clonotype_id':'clonotype_id'},inplace=True)
+    
+    df_merge_f = pd.merge(df_merge,clonotype,on='clonotype_id')
+    df_merge_f['chain'] = df_merge_f['v_call'].apply(lambda x:x[:3])
+    df_merge_f['id_num'] = df_merge_f['clonotype_id'].apply(lambda x: int(x[9:]))
+    df_merge_f.sort_values(['id_num','chain'],inplace=True)
+    
+    df_merge_f.drop_duplicates(['clonotype_id','chain'],inplace=True)
+    df_merge_f = df_merge_f.where(df_merge_f.notnull(),'None')
+    clonotype_id_list = list(df_merge_f.clonotype_id.unique())
+    
+    outfile = open('./anno_seq_aa.fasta','w')
+    outfile.write("Clonotype(clonotype_id|V-gene|D-gene|J-gene|CDR3|C-gene|)\tCell_Number\tCgene\tSequence\n")
+    for i in clonotype_id_list:
+        df_tmp = df_merge_f[df_merge_f['clonotype_id']==i]
+        outfile.write(f'{list(df_tmp.clonotype_id)[0]}|')
+        for i in range(df_tmp.shape[0]):
+            outfile.write(f'{list(df_tmp.v_call)[i]}|{list(df_tmp.d_call)[i]}|{list(df_tmp.j_call)[i]}|{list(df_tmp.cdr3)[i]}|{list(df_tmp.c_call)[i]}|')
+        outfile.write(f'\t{list(df_tmp.frequency)[0]}\t')
+        outfile.write('|'.join(list(df_tmp.c_call)))
+        outfile.write('\n')
+        for i in range(df_tmp.shape[0]):
+            outfile.write(f'ChianType:{list(df_tmp.chain)[i]}\n{list(df_tmp.sequence_aa)[i]}\n')
+    outfile.close()
+
+
+    outfile = open('./anno_seq_nt.fasta','w')
+    outfile.write("Clonotype(clonotype_id|V-gene|D-gene|J-gene|CDR3|C-gene|)\tCell_Number\tCgene\tSequence\n")
+    for i in clonotype_id_list:
+        df_tmp = df_merge_f[df_merge_f['clonotype_id']==i]
+        outfile.write(f'{list(df_tmp.clonotype_id)[0]}|')
+        for i in range(df_tmp.shape[0]):
+            outfile.write(f'{list(df_tmp.v_call)[i]}|{list(df_tmp.d_call)[i]}|{list(df_tmp.j_call)[i]}|{list(df_tmp.cdr3)[i]}|{list(df_tmp.c_call)[i]}|')
+        outfile.write(f'\t{list(df_tmp.frequency)[0]}\t')
+        outfile.write('|'.join(list(df_tmp.c_call)))
+        outfile.write('\n')
+        for i in range(df_tmp.shape[0]):
+            outfile.write(f'ChianType:{list(df_tmp.chain)[i]}\n{list(df_tmp.sequence)[i]}\n')
+    outfile.close()
+
+
+    df_h = contig_for_plot[contig_for_plot['chain']=='IGH']
+    tmp_dict = df_h.groupby('c_gene')['raw_clonotype_id'].count().to_dict()
+    try:
+        del(tmp_dict['None'])
+    except KeyError:
+        pass
+
+    tmp_dict,reverse_tmp_dict = sorted(tmp_dict.items(),key=lambda x:x[1]), sorted(tmp_dict.items(),key=lambda x:x[1], reverse=True)
+    
+    label, value = [], []
+    if len(tmp_dict) % 2 == 0:
+        for i in range(len(tmp_dict)//2):
+            label.append(tmp_dict[i][0])
+            label.append(reverse_tmp_dict[i][0])
+            value.append(tmp_dict[i][1])
+            value.append(reverse_tmp_dict[i][1])
+    else:
+        for i in range(len(tmp_dict)//2):
+            label.append(tmp_dict[i][0])
+            label.append(reverse_tmp_dict[i][0])
+            value.append(tmp_dict[i][1])
+            value.append(reverse_tmp_dict[i][1])
+        label.append(tmp_dict[len(tmp_dict)//2][0])
+        value.append(tmp_dict[len(tmp_dict)//2][1])
+    
+    plt.pie(value, labels=label,autopct='%.2f%%',pctdistance=0.8,explode=(0.1,0.1,0.1,0.1))
+    plt.title('IG types')
+    plt.savefig('./IGH.png', dpi=280)
+
+
+if __name__ == '__main__':
+    cr_out = sys.argv[1]
+    get_anno_seq(cr_out)
+    
+    
---
+++ get_anno_seq/run.sh
@@ -0,0 +1,1 @@
+python //SGRNJ03/randd/cjj/Script/get_anno_seq/anno_seq.py /SGRNJ06/randd/USER/cjj/celedev/vdj10x/20220629no_match/BCRLFBVC220616037X1CK
