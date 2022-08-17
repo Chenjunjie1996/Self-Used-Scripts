9fe7e8d219365d1427f7f79783d4ae599a564bd2
陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>
2022-08-17 16:58:49 +0800

    mapping class

---
+++ mappingclass/yingshe.R
@@ -0,0 +1,69 @@
+library(Seurat)
+library(tidyverse)
+library(argparse)
+library(ggplot2)
+library(stringr)
+library(dplyr)
+
+parser <- ArgumentParser()
+parser$add_argument("--sample",help="sample name", required=TRUE)
+parser$add_argument("--rds", help="scRNA rds path", required=TRUE)
+parser$add_argument("--VDJ", help='VDJ match_contigs.csv path', required=TRUE)
+parser$add_argument("--outdir", help='out dir', required=TRUE)
+args <- parser$parse_args()
+
+rds <- readRDS(args$rds)
+
+Idents(rds) <- "orig.ident"
+rna <- subset(rds,idents = c(args$sample))
+
+if (grepl("confident_count.tsv", args$VDJ)){
+  vdj <- read.table(args$VDJ, sep='\t', header=T)
+}else{
+  vdj <- read.table(args$VDJ, sep=',', header=T)
+}
+
+df <- rna@meta.data
+df$barcode <- rownames(df)
+
+if (grepl("_", rownames(df)[1])){
+  vdj$shi<-paste(args$sample,"_",sep="")
+  vdj$barcode<-str_c(vdj$shi,vdj$barcode)
+}
+
+cells <- vdj
+barcodes <- unique(cells$barcode)
+
+filter_df <- filter(df, barcode %in% barcodes)
+res <- table(filter_df$seurat_clusters)
+res <- as.data.frame(res)
+
+out_csv = stringr::str_glue("{args$outdir}/match_barcodes_celltypes_distribution.txt")
+write_csv(res,file=out_csv)
+
+meta = rna@meta.data
+meta$Class = 'NA'
+meta[barcodes,'Class'] = 'T/BCR'
+meta <- meta %>% drop_na(nCount_RNA)
+table(meta$Class == 'T/BCR')
+rna@meta.data = meta
+rna <- RunUMAP(rna, dims = 1:20)
+
+outP = stringr::str_glue("{args$outdir}/cluster_umap.png")
+png(outP, height=1000, width=1000)
+UMAPPlot(rna,group.by='seurat_clusters',label=TRUE)
+dev.off()
+
+outP1 = stringr::str_glue("{args$outdir}/umapplot.png")
+png(outP1, height=1000, width=1000)
+UMAPPlot(rna,group.by='Class',cols=c('grey','red'),label=TRUE)
+dev.off()
+
+outP2 = stringr::str_glue("{args$outdir}/assign.png")
+png(outP2, height=1000, width=1000)
+UMAPPlot(rna,group.by='new_ident',label=TRUE,label.box=TRUE)
+dev.off()
+
+meta = rna@meta.data
+outMeta = stringr::str_glue("{args$outdir}/meta.csv")
+write.csv(meta,outMeta)
---
+++ mappingclass/yingshe.py
@@ -0,0 +1,138 @@
+import os
+import subprocess
+import glob
+import pandas as pd
+import sys
+from concurrent.futures import ProcessPoolExecutor
+import xlwt
+import os
+import re
+import celescope.tools.utils as utils
+
+
+# HumanPrimaryCellAtlasData and MouseRNAseqData dataset
+CELL_TYPE_DICT = {
+    'TCR':['TCELLS', 'TCELL' 'NKTCELLS'],
+    'BCR':['MATUREBCELL', 'PLASMACELLS', 'BCELLS', 'BCELL', 'PREBCELLCD34'],
+    }
+ROOT_DIR = os.path.dirname(__file__)
+OUT_PATH = 'YingShe'
+
+
+class Yinshe:
+    def __init__(self, sample, rds, VDJ_file, outdir) -> None:
+        self.sample = sample
+        self.rds = rds
+        self.VDJ_file = VDJ_file
+        self.outdir = outdir
+    
+    @utils.add_log
+    def run_mapping(self):
+        cmd = (
+            f'Rscript {ROOT_DIR}/yingshe.R '
+            f'--sample {self.sample} '
+            f'--rds {self.rds} '
+            f'--VDJ {self.VDJ_file} '
+            f'--outdir {self.outdir} '
+        )
+        subprocess.check_call(cmd, shell=True)
+    
+    def run(self):
+        self.run_mapping()
+
+
+def parse_mapfile(mapfile):
+
+    df_mapfile = pd.read_csv(mapfile, sep='\t')
+    path_list = df_mapfile['path']
+    rds_list = df_mapfile['rds']
+    sample_list = df_mapfile['sample']
+
+    return path_list, rds_list, sample_list
+
+
+def mapping(path, rds, sample):
+    outdir = f'{OUT_PATH}/{sample}'
+    utils.check_mkdir(outdir)
+    
+    if os.path.exists(f'{path}/05.match'): # cellranger
+        try:
+            VDJ_file = glob.glob(f'{path}/05.match/match_contigs.csv')[0] # 3.0, 4.0, 6.0
+        except IndexError:
+            VDJ_file = glob.glob(f'{path}/05.match/matched_contig_annotations.csv')[0] # 7.0
+    
+    elif os.path.exists(f'{path}/04.summarize'): # trust4
+        VDJ_file = glob.glob(f'{path}/04.summarize/*_filtered_contig.csv')[0]
+    
+    elif os.path.exists(f'{path}/05.count_vdj'):
+        VDJ_file = glob.glob(f'{path}/05.count_vdj/*_cell_confident_count.tsv')[0]
+
+    runner = Yinshe(sample, rds, VDJ_file, outdir)
+    runner.run()
+
+
+def get_seqtype(path_list):
+    run_shell = glob.glob(f'{path_list[0]}/*.mapfile')
+    with open(run_shell) as f:
+        for line in f.readlines():
+            if 'BCR' in line:
+                return 'BCR'
+            return 'TCR'
+
+
+def run_count(mapping_cell_type):
+    meta_list = glob.glob(f"./{OUT_PATH}/*/meta.csv")
+    sample_list = [meta.split('/')[-2] for meta in meta_list]
+
+    report = xlwt.Workbook()
+    sheet = report.add_sheet('mapping count')
+
+    row0 = ['number of mapping to T/B cells', 'number of T/B cells', 'percent']
+    row1 = sample_list
+
+    mapping_count, ZL_count = [], []
+    for meta in meta_list:
+        df = pd.read_csv(meta)
+
+        if 'new_ident' in df.columns: # manual assign column name
+            ident = 'new_ident'
+        elif 'celltype' in df.columns: # singleR column name
+            ident = 'celltype'
+
+        df[ident] = df[ident].apply(lambda x: re.sub(r"[^a-zA-Z0-9]","", x).upper())
+        df_count = df[df[ident].isin(mapping_cell_type)]
+
+        mapping_count.append(int(df_count['Class'].value_counts()))
+        ZL_count.append(int(df_count[ident].value_counts()))
+
+    percent = [x / y for x, y in zip(mapping_count, ZL_count)]
+    percent = [str(round(i,4)*100) + "%" for i in percent]
+
+    for _i in range(len(row0)):
+        sheet.write(0, _i+1, row0[_i])
+    for _i in range(len(row1)):
+        sheet.write(_i+1, 0, row1[_i])
+        sheet.write(_i+1, 1, mapping_count[_i])
+        sheet.write(_i+1, 2, ZL_count[_i])
+        sheet.write(_i+1, 3, percent[_i])
+
+    report.save(f'./{OUT_PATH}/mapping_count.xls')
+    return report
+
+
+def main():
+    os.system(f'mkdir {OUT_PATH}')
+    mapfile = sys.argv[1]
+    path_list, rds_list, sample_list = parse_mapfile(mapfile)
+
+    with ProcessPoolExecutor(max_workers=10) as executor:
+        for result in executor.map(mapping, path_list, rds_list, sample_list):
+            print(result, 'done')
+    
+    mapping_cell_type = CELL_TYPE_DICT[get_seqtype(path_list)]
+    run_count(mapping_cell_type)
+
+
+if __name__ == '__main__':
+    main()
+
