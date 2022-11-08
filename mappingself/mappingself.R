81f49246d4ee9f62937757dba67ab38ed1989b6c
陈俊杰 <chenjunjie@SGRNJlogin.sgr.com>
2022-11-08 14:50:59 +0800

    mappingself

--- mappingself/mappingself.R
+++ mappingself/mappingself.R
@@ -16,6 +16,9 @@ args <- parser$parse_args()
 rds <- readRDS(args$rds)
 vdj <- read.table(args$VDJ, sep=',', header=T)
 
+df <- rds@meta.data
+df$barcode <- rownames(df)
+
 if (grepl("_", rownames(df)[1])){
   vdj$shi<-paste(args$sample,"_",sep="")
   vdj$barcode<-str_c(vdj$shi,vdj$barcode)
@@ -23,11 +26,9 @@ if (grepl("_", rownames(df)[1])){
 
 cells <- subset(vdj, productive=='True')
 barcodes <- unique(cells$barcode)
-df <- rds@meta.data
-df$barcode <- rownames(df)
 
 filter_df <- filter(df, barcode %in% barcodes)
-res <- table(filter_df$celltype)
+res <- table(filter_df$cluster)
 res <- as.data.frame(res)
 
 out_csv = stringr::str_glue("{args$outdir}/{args$sample}_match_barcodes_celltypes_distribution.txt")
@@ -35,14 +36,20 @@ write_csv(res,file=out_csv)
 
 meta = rds@meta.data
 meta$Class = 'NA'
-meta[barcodes,'Class'] = 'T/BCR'
+meta$barcode = rownames(meta)
+meta = meta %>%
+  mutate(Class = if_else(barcode %in% barcodes,
+                         true = "T/BCR",
+                         false = "NA"))
+meta = select(meta, -c("barcode"))
 meta <- meta %>% drop_na(nCount_RNA)
 table(meta$Class == 'T/BCR')
 rds@meta.data = meta
+rds <- RunUMAP(rds, dims = 1:20)
 
 outP = stringr::str_glue("{args$outdir}/{args$sample}_cluster_umap.png")
 png(outP, height=1000, width=1000)
-UMAPPlot(rds,group.by='seurat_clusters',label=TRUE)
+UMAPPlot(rds,group.by='seurat_cluster',label=TRUE)
 dev.off()
 
 outP1 = stringr::str_glue("{args$outdir}/{args$sample}_umapplot.png")
@@ -54,8 +61,10 @@ outP2 = stringr::str_glue("{args$outdir}/{args$sample}_assign.png")
 png(outP2, height=1000, width=1000)
 if ('new_ident' %in% colnames(meta)){
   UMAPPlot(rds,group.by='new_ident',label=TRUE,label.box=TRUE)
-}else{
+}else if ('celltype' %in% colnames(meta)){
   UMAPPlot(rds,group.by='celltype',label=TRUE,label.box=TRUE)
+}else {
+  UMAPPlot(rds,group.by='cluster',label=TRUE,label.box=TRUE)
 }
 dev.off()
 
