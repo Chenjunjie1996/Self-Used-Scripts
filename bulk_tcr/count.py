8f57a23f9d1e98cf8cda6a0e7edf66fa3543ba03
陈俊杰 <chenjunjie@singleronbio.com>
2023-07-13 17:18:11 +0800

    update

--- bulk_tcr/count.py
+++ bulk_tcr/count.py
@@ -7,16 +7,18 @@ class Count:
     Count types, frequencies, diversity of different index.
     """
     def __init__(self):
-        self.df_clonotypes = glob.glob("*/05.count_vdj/*_clonetypes.csv")[0]
-        self.out_file = "clonotypes_info.txt"
+        self.df_clonotypes = glob.glob("*/05.count_vdj/*_clonetypes.csv")
     
     def __call__(self):
-        df = pd.read_csv(self.df_clonotypes)
         
-        df.groupby("Index").agg({'Frequency':'sum','ClonotypeID':'count','Diversity':'min'})\
-            .reset_index().rename(columns={'Frequency':'Count','ClonotypeID':'Types'})\
-                .sort_values("Diversity", ascending=False)\
-                    .to_csv(self.out_file, sep='\t', index=False)
+        for df in self.df_clonotypes:
+            out_file = f"{df.split('/')[0]}_info.txt"
+            df = pd.read_csv(df)
+            
+            df = df.groupby("Index").agg({'Frequency':'sum','ClonotypeID':'count','Diversity':'min'})\
+                .reset_index().rename(columns={'Frequency':'Count','ClonotypeID':'Types'})\
+                    .sort_values("Diversity", ascending=False)\
+                        .to_csv(out_file, sep='\t', index=False)
 
 
 if __name__ == '__main__':
