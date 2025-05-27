87f6c19c0c2aa5c4314003f46c4dad7f38b1aaa0
陈俊杰 <chenjunjie@singleronbio.com>
2025-05-27 17:49:09 +0800

    add mobiuscope intersection count

---
+++ mobiuscope/intersection.py
@@ -0,0 +1,67 @@
+import argparse
+import pysam
+
+
+class Filter_bam:
+    """
+    """
+    def __init__(self, args):
+        self.args = args
+        
+        # in
+        self.bam = args.bam
+        
+        # out
+        self.set3 = set()
+        self.set5 = set()
+        self.total_count = 0
+        self.count3 = 0
+        self.count5 = 0
+        self.intersec_count = 0
+        self.count_file = "bc_umi_count.txt"
+        self.set3_file = open("set3p.txt", 'w')
+        self.set5_file = open("set5p.txt", 'w')
+    
+    
+    def __call__(self):
+        inbam = pysam.AlignmentFile(self.bam, "rb")
+        
+        for read in inbam:
+            cb = read.get_tag("CB")
+            umi = read.get_tag("UB")
+            judge = read.query_name.split(':')[-1]
+            self.total_count += 1
+            if judge == '5p':
+                self.count5 += 1
+                self.set5.add( (cb, umi) )
+            else:
+                self.count3 += 1
+                self.set3.add( (cb, umi) )
+        self.intersec_count = len(self.set5.intersection(self.set3))
+                
+        inbam.close()
+        
+        with open(self.count_file, 'w') as fp:
+            fp.write(f"total read count : {self.total_count}\n")
+            fp.write(f"3p read count : {self.count3}\n")
+            fp.write(f"5p read count : {self.count5}\n")
+            fp.write(f"3p bc_umi combination : {len(self.set3)}\n")
+            fp.write(f"5p bc_umi combination : {len(self.set5)}\n")
+            fp.write(f"3p5p bc_umi intersection : {self.intersec_count}\n")
+        
+        
+        for i in self.set3:
+            self.set3_file.write(f"{':'.join(i)}\n")
+            
+        for i in self.set5:
+            self.set5_file.write(f"{':'.join(i)}\n")
+                    
+        self.set3_file.close()
+        self.set5_file.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--bam", help="bam file", required=True)
+    args = parser.parse_args()
+    Filter_bam(args)()
