19e2eced3c6e11b6a8baa1a2238b1113194805f0
陈俊杰 <chenjunjie@singleronbio.com>
2025-07-23 10:03:06 +0800

    update check cell num

--- check_cell_num.py
+++ check_cell_num.py
@@ -5,11 +5,17 @@ import glob
 from celescope.tools import utils
 
 def parse_file(filter_contig, transc):
-    filter_contig = glob.glob(f'{filter_contig}/06.summarize/filtered_contig_annotations.csv')[0]
+    """
+    usage
+    python /SGRNJ06/randd/USER/cjj/Script/Self-Used-Scripts/check_cell_num.py\
+    /SGRNJ06/randd/PROJECT/IR_group/AccuraCode_TCR/2025sCircle_VDJ/20250717_LJ_Hum_GOTN/TCR2/GTBSA05038x2_TCR\
+    /SGRNJ06/randd/PROJECT/IR_group/AccuraCode_TCR/2025sCircle_VDJ/20250717_LJ_Hum_GOTN/RNA/GTBSA05039x2_RNA
+    """
+    filter_contig = glob.glob(f'{filter_contig}/*/filtered_contig_annotations.csv')[0]
     filter_contig = pd.read_csv(filter_contig)
     cell_nums = len(set(filter_contig['barcode'].tolist()))
 
-    match_cell_barcodes, _match_cell_number = utils.read_barcode_file(transc)
+    match_cell_barcodes, _match_cell_number = utils.get_barcode_from_match_dir(transc)
 
     sgr_cbs = set(filter_contig['barcode'].tolist())
     df_sgr = pd.DataFrame(match_cell_barcodes, columns=['barcode'])
